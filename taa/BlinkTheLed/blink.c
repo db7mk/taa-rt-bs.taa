c6413f6693318524d414c0ddce6d806aaf65a80a
Markus <Markus@192.168.192.13>
2014-01-12 16:18:29 +0100

    BlinkTheLed mit einem Beispiel für die add von verschiedenen Datentypen hinzugefügt

--- taa/BlinkTheLed/blink.c
+++ taa/BlinkTheLed/blink.c
@@ -20,9 +20,33 @@
 
 //Das ist jetzt die erste Version des Programms
 
-#include <msp430.h>				
+#include <msp430.h>
+#include <float.h>
+
+
+char addChar(char a, char b)
+{
+	char r;
+	r = a + b ;
+	return r;
+}
+
+long addLong(long a, long b)
+{
+	long  r;
+	r = a + b ;
+	return r;
+}
+
+float addFloat(float a, float b)
+{
+float r;
+r = a + b;
+return r;
+}
 
 int main(void) {
+	long r;
 	WDTCTL = WDTPW | WDTHOLD;		// Stop watchdog timer
 	P1DIR |= 0x01;					// Set P1.0 to output direction
 
@@ -34,6 +58,11 @@ int main(void) {
 		i = 10000/2;				// Blinke doppelt so schnell
 		do i--;
 		while(i != 0);
+		//Berechne etwas
+		r = addChar(1, 5);
+		r = addLong(1, 5);
+		r = (long) addFloat(1.1,5.1);
+		P1OUT ^= ((unsigned char)r) &0xFF;
 	}
 	
 	return 0;
