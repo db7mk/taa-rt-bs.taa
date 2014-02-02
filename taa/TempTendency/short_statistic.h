052895902545287fe94978a45e9dcbbc26266730
mkleinselbeck <mkleinselbeck@gmail.com>
2014-02-02 10:01:24 +0000

    integer statistic

---
+++ taa/TempTendency/short_statistic.h
@@ -0,0 +1,13 @@
+/*!
+ * short_statistic.h
+ *
+ *  Created on: 25.01.2014
+ *      Author: dev
+ */
+
+#ifndef SHORT_STATISTIC_H_
+#define SHORT_STATISTIC_H_
+
+extern unsigned short calcAverage(unsigned short* array,unsigned char size);
+
+#endif /* SHORT_STATISTIC_H_ */
