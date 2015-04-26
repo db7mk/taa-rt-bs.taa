ea2d941d168bbedbf10ab8a90bb1292e1472373e
am <am@am-desktop>
2015-04-26 16:56:55 +0200

    Toggle internal clock with button

--- taa/TogglePort/TogglePort/TogglePort.c
+++ taa/TogglePort/TogglePort/TogglePort.c
@@ -1,57 +1,40 @@
 #include <msp430.h>
 
+unsigned short volatile buttonPressed;
 
 
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1(void) {
 
+		P1IFG &= ~0x08; 					// P1.3 Interrupt Flag cleared
+		buttonPressed++;
 
-#define TOGGLE_LOW
-
-
-//#undef TOGGLE_LOW
-/*
- * Controller läuft mit dem internen Oscillator 1MHz
- * ref: http://www.ccs.neu.edu/home/noubir/Courses/CSU610/S07/MSP430-Clock-Timers.pdf
- */
-void togglePort1_1MHz()
-{
-	//Interner Oscilator 1MHZ
-	BCSCTL1 = CALBC1_1MHZ;			/* Basic Clock System Control 1 */
-	DCOCTL = CALDCO_1MHZ;			/* DCO Clock Frequency Control */
-
-	P1DIR |= 0x01;					// Set P1.0 to output direction
-	for(;;)
-		P1OUT ^= 0x01;				// Toggle P1.0 using exclusive-OR
-}
-
-
-/*
- * Controller läuft mit dem internen Oscillator 16MHz
- * ref: http://www.ccs.neu.edu/home/noubir/Courses/CSU610/S07/MSP430-Clock-Timers.pdf
- */
-void togglePort1_16Mhz()
-{
-	//Interner Oscilator 16MHZ
-	BCSCTL1 = CALBC1_16MHZ;			/* Basic Clock System Control 1 */
-	DCOCTL = CALDCO_16MHZ;			/* DCO Clock Frequency Control */
-
-
-	P1DIR |= 0x01;					// Set P1.0 to output direction
-	for(;;)
-		P1OUT ^= 0x01;				// Toggle P1.0 using exclusive-OR
+		if (buttonPressed & 0x01) {
+			BCSCTL1 = CALBC1_1MHZ; 			/* Basic Clock System Control 1 */
+			DCOCTL = CALDCO_1MHZ; 			/* DCO Clock Frequency Control */
+		} else {
+			BCSCTL1 = CALBC1_16MHZ; 		/* Basic Clock System Control 1 */
+			DCOCTL = CALDCO_16MHZ; 			/* DCO Clock Frequency Control */
+		}
 }
 
 
 
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;	// Stop G timer
+    WDTCTL = WDTPW | WDTHOLD;		// Stop Watchdog
+	P1DIR |= 0x01;					// Set P1.0 to output direction
+	P1SEL &= ~0x08;					// Select Port 1 P1.3 (push button)
+    P1DIR &= ~0x08;					// Port 1 P1.3 (push button) as input, 0 is input
+	P1REN |= 0x08;					// Enable Port P1.3 (push button) pull-up resistor
 
-#ifdef TOGGLE_LOW
-    togglePort1_1MHz();
-#endif
+	P1IE |= 0x08;					// Port 1 Interrupt Enable P1.3 (push button)
 
-#ifndef TOGGLE_LOW
-    togglePort1_16Mhz();
-#endif
+	P1IFG &= ~0x08;					// Clear interrupt flag
 
+	_BIS_SR(GIE);		           	// Enable interrupts
+	buttonPressed=0;
+
+	for(;;)
+		P1OUT ^= 0x01;				// Toggle P1.0 using exclusive-OR
 	return 0;
 }
